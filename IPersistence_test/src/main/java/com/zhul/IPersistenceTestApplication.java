0f19e52972ebcec0fa07fd7cc3ec4b1dac04d61b
zl1509015035 <1509015035@qq.com>
2021-11-01 23:01:20 +0800

    Initial commit

---
+++ IPersistence_test/src/main/java/com/zhul/IPersistenceTestApplication.java
@@ -0,0 +1,13 @@
+package com.zhul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPersistenceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IPersistenceTestApplication.class, args);
+    }
+
+}
---
+++ IPersistence_test/src/main/java/com/zhul/pojo/User.java
@@ -0,0 +1,8 @@
+package com.zhul.pojo;
+
+/**
+ * @author juanwang
+ * @create 2021/11/1 22:42
+ */
+public class User {
+}
